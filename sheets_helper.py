710912327599f0ddb6a6176aa7ee277e1a995661
joesam000 <34879356+joesam000@users.noreply.github.com>
2025-08-21 09:48:33 +0700

    Create sheets_helper.py

---
+++ sheets_helper.py
@@ -0,0 +1,25 @@
+import gspread
+import os, pickle
+from google.auth.transport.requests import Request
+from google_auth_oauthlib.flow import InstalledAppFlow
+
+SCOPES = ["https://www.googleapis.com/auth/spreadsheets.readonly"]
+
+def get_sheet(sheet_name):
+    creds = None
+    if os.path.exists("token.pickle"):
+        with open("token.pickle", "rb") as token:
+            creds = pickle.load(token)
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                "credentials.json", SCOPES
+            )
+            creds = flow.run_local_server(port=0)
+        with open("token.pickle", "wb") as token:
+            pickle.dump(creds, token)
+
+    gc = gspread.authorize(creds)
+    return gc.open(sheet_name).sheet1
